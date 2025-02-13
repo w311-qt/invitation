2e98e8d6b972d54ffca1f72145bcdec457be7317
W311 <90219415+w311-qt@users.noreply.github.com>
2025-02-13 17:17:39 +0300

    Update index.js

--- index.js
+++ index.js
@@ -23,4 +23,18 @@ $(document).ready(function() {
             .removeClass("open");
     }
 
+    // Обработчик для кнопки "Открыть"
+    $("#open").click(function() {
+        $("#envelope").removeClass("close").addClass("open");
+        // Показываем кнопку программы
+        $(".program-button").removeClass("hidden");
+    });
+
+    // Обработчик для кнопки "Закрыть"
+    $("#reset").click(function() {
+        $("#envelope").removeClass("open").addClass("close");
+        // Скрываем кнопку программы
+        $(".program-button").addClass("hidden");
+    });
+
 })
